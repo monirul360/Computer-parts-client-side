cfa25eabbbd5af6caa6fe5a722399cae2daa41e7
monirul360 <monirulislam65361@gmail.com>
2022-05-25 01:49:51 +0600

    parts page design and api call

---
+++ src/Page/Home/Parts-row/Parts-row.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Partsrow = ({ parts }) => {
+    const { name, description, quantity, available, price, image } = parts;
+    const sliceDes = description.slice(0, 100);
+    return (
+        <div>
+            <div class="card m-5 bg-base-100 shadow-xl">
+                <figure><img src={image} alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{name}</h2>
+                    <p>{sliceDes}</p>
+                    <p>Quantity : {quantity}</p>
+                    <p>Available : {available}</p>
+                    <p className='text-red-600 font-bold'>Price : ${price}</p>
+                    <div class="card-actions justify-end">
+                        <Link to='/purchase/id' class="btn btn-primary btn-sm">Buy Now</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Partsrow;
---
+++ src/Page/Home/Parts/Parts.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../../Loading/Loading';
+import Partsrow from '../Parts-row/Parts-row';
+
+const Parts = () => {
+    const { data: parts, isLoading, refetch } = useQuery("parts", () => fetch('http://localhost:5000/perts').then(res => res.json()))
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    const sliceParts = parts.slice(0, 6);
+    return (
+        <div>
+            <p className='text-3xl text-center  p-3'>Our parts</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1'>
+                {
+                    sliceParts.map(parts => <Partsrow
+                        key={parts._id}
+                        parts={parts}
+                    ></Partsrow>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Parts;
