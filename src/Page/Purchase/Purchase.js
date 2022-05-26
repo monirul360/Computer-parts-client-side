d7b1ae6a89e860c833795fff0ceac6beb7198aa4
monirul360 <monirulislam65361@gmail.com>
2022-05-26 06:09:17 +0600

    purchas page setup and order info database save

--- src/Page/Purchase/Purchase.js
+++ src/Page/Purchase/Purchase.js
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import auth from '../../Firebase-init';
 
 const Purchase = () => {
+    const navigate = useNavigate();
+    const [user] = useAuthState(auth);
     const { id } = useParams();
     const url = `http://localhost:5000/perts/${id}`
     const [parts, setParts] = useState([]);
@@ -10,23 +16,81 @@ const Purchase = () => {
             .then(res => res.json())
             .then(data => setParts(data));
     }, [])
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    const price = parts?.price;
+    const quantity = parts?.quantity;
+    const totlaPrice = price * quantity;
+    const onSubmit = async (data) => {
+        const booking = {
+            name: parts.name,
+            productid: parts._id,
+            username: data.name,
+            address: data.address,
+            email: data.email,
+            totlaPrice: totlaPrice,
+            number: data.number,
+        }
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(inserted => {
+                if (inserted.insertedId) {
+                    toast.success('Order successfull');
+                    reset();
+                    navigate('/dashboard/')
+                }
+                else {
+                    toast.error('Failed to Order');
+                }
+            })
+    }
     return (
         <div>
-            <div className="hero p-5 bg-base-200">
-                <div className="hero-content flex-col lg:flex-row">
-                    <img src={parts.image} className="max-w-sm rounded-lg shadow-2xl" alt='' />
-                    <div>
-                        <h1 className="text-5xl font-bold">{parts.name}</h1>
-                        <p className="py-6">{parts.description}</p>
-                        <p className='py-2'>Quantity : {parts.quantity}</p>
-                        <p className='pb-3'>Available : {parts.available}</p>
-                        <button className="btn btn-primary">Get Started</button>
-                        <form>
-
-                        </form>
-                    </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-10 gap-10 items-center '>
+                <div>
+                    <img src={parts.image} alt="" />
+                    <h1 className="text-3xl py-3 font-bold">{parts.name}</h1>
+                    <p className="py-2">{parts.description}</p>
+                    <p className='py-2'>Price : ${parts.price}</p>
+                    <p className='py-2'>Quantity : {parts.quantity}</p>
+                    <p className='pb-3'>Available : {parts.available}</p>
+                    <form>
+                        <input type="number" name='Add Quantity' placeholder='Add ' className="input input-bordered w-full max-w-xs" />
+                    </form>
+                </div>
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <label className="label">
+                            <span className="label-text">Address</span>
+                        </label>
+                        <input type="name" placeholder="Address" className="input input-bordered w-full max-w-xs" {...register("address", { required: true })} />
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="name" readOnly value={user?.displayName} className="input input-bordered w-full max-w-xs" {...register("name", { required: true })} />
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" value={user?.email} readOnly className="input input-bordered w-full max-w-xs" {...register("email", { required: true })} />
+                        <label className="label">
+                            <span className="label-text">Price</span>
+                        </label>
+                        <input type="number" readOnly value={totlaPrice} className="input input-bordered w-full max-w-xs" {...register("price", { required: true })} />
+                        <label className="label">
+                            <span className="label-text">Number</span>
+                        </label>
+                        <input type="number" placeholder="Number" class="input input-bordered w-full max-w-xs" {...register("number", { required: true })} />
+                        <br />
+                        <input className='input input-bordered bg-primary text-white w-full my-3  max-w-xs' type="submit" value="Order" />
+                    </form>
                 </div>
             </div>
+
         </div>
     );
 };
